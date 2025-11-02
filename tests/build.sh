fd10f154818bec802397c2baf56e59feb10ec11f
Joseph Amsellem <79039145+AmsellemJoseph@users.noreply.github.com>
2025-11-02 08:46:27 +0200

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+mv ./Dockerfile ./Dockerfile.old
+cp ./.docker/app/Dockerfile ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/glpi:latest | docker load
